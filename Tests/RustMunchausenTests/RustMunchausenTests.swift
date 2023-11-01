dd81b532ed5d6b9e28aff92c8f2bb93ee995a5c8
Joshua Buhler <josh.buhler@snapone.com>
2023-11-01 15:33:14 -0600

    Initial Commit

---
+++ Tests/RustMunchausenTests/RustMunchausenTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import RustMunchausen
+
+final class RustMunchausenTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
